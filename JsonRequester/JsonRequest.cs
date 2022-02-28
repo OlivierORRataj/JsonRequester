5bf931e46c220acd24b581e58b86b0b4e758dd11
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-02-28 16:29:09 +0100

    JsonRequest.cs

---
+++ JsonRequester/JsonRequest.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json;
+
+namespace JsonRequester
+{
+    public class JsonRequest
+    {
+        public async Task<T> GET<T>(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                using (var response = await client.GetAsync(url))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+
+                    return (T)JsonSerializer.Deserialize<T>(apiResponse);
+                        
+                }
+            }
+        }
+
+        public async Task<T> GET<T>(string url, bool ignoreCaseSensitive)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                using (var response = await client.GetAsync(url))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+
+                    return (T)JsonSerializer.Deserialize<T>(apiResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = ignoreCaseSensitive });
+                        
+                }
+            }
+        }
+
+        public async Task<T> GET<T>(string url, string apikeyHeader, string apikey)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add(apikeyHeader, apikey);
+                using (var response = await client.GetAsync(url))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+
+                    return (T)JsonSerializer.Deserialize<T>(apiResponse);
+                        
+                }
+            }
+        }
+
+        public async Task<T> GET<T>(string url, string apikeyHeader, string apikey, bool ignoreCaseSensitive)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add(apikeyHeader, apikey);
+                using (var response = await client.GetAsync(url))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+
+                    return (T)JsonSerializer.Deserialize<T>(apiResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = ignoreCaseSensitive });
+                        
+                }
+            }
+        }
+    }
+}
