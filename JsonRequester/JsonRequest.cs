6fc3a4d3cbc1b7a07e88cbbf0660dcf0b7092806
Olivier Rataj <olivier.rataj@edu.newton.se>
2022-02-28 16:36:28 +0100

    Add comment for each GET method

--- JsonRequester/JsonRequest.cs
+++ JsonRequester/JsonRequest.cs
@@ -4,6 +4,12 @@ namespace JsonRequester
 {
     public class JsonRequest
     {
+        /// <summary>
+        /// Deserializes the provided url and returns it as <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type to return</typeparam>
+        /// <param name="url">The url to fetch the Json from</param>
+        /// <returns><typeparamref name="T"/></returns>
         public async Task<T> GET<T>(string url)
         {
             using (var client = new HttpClient())
@@ -19,6 +25,13 @@ namespace JsonRequester
             }
         }
 
+        /// <summary>
+        /// Deserializes the provided url and returns it as <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type to return</typeparam>
+        /// <param name="url">The url to fetch the Json from</param>
+        /// <param name="ignoreCaseSensitive">If set to true, the deserialization will ignore case sensitivity.</param>
+        /// <returns><typeparamref name="T"/></returns>
         public async Task<T> GET<T>(string url, bool ignoreCaseSensitive)
         {
             using (var client = new HttpClient())
@@ -34,12 +47,20 @@ namespace JsonRequester
             }
         }
 
-        public async Task<T> GET<T>(string url, string apikeyHeader, string apikey)
+        /// <summary>
+        /// Deserializes the provided url and returns it as <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type to return</typeparam>
+        /// <param name="url">The url to fetch the Json from</param>
+        /// <param name="apikeyHeader">The header of the key (field)</param>
+        /// <param name="apikeyValue">The header of the key (value)</param>
+        /// <returns><typeparamref name="T"/></returns>
+        public async Task<T> GET<T>(string url, string apikeyHeader, string apikeyValue)
         {
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
-                client.DefaultRequestHeaders.Add(apikeyHeader, apikey);
+                client.DefaultRequestHeaders.Add(apikeyHeader, apikeyValue);
                 using (var response = await client.GetAsync(url))
                 {
                     var apiResponse = await response.Content.ReadAsStringAsync();
@@ -49,13 +70,21 @@ namespace JsonRequester
                 }
             }
         }
-
-        public async Task<T> GET<T>(string url, string apikeyHeader, string apikey, bool ignoreCaseSensitive)
+        /// <summary>
+        /// Deserializes the provided url and returns it as <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">The type to return</typeparam>
+        /// <param name="url">The url to fetch the Json from</param>
+        /// <param name="apikeyHeader">The header of the key (field)</param>
+        /// <param name="apikeyValue">The header of the key (value)</param>
+        /// <param name="ignoreCaseSensitive">If set to true, the deserialization will ignore case sensitivity.</param>
+        /// <returns><typeparamref name="T"/></returns>
+        public async Task<T> GET<T>(string url, string apikeyHeader, string apikeyValue, bool ignoreCaseSensitive)
         {
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Add("Accept", "application/json");
-                client.DefaultRequestHeaders.Add(apikeyHeader, apikey);
+                client.DefaultRequestHeaders.Add(apikeyHeader, apikeyValue);
                 using (var response = await client.GetAsync(url))
                 {
                     var apiResponse = await response.Content.ReadAsStringAsync();
